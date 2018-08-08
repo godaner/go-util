3c7cada47c2e0a35d24af318ce0eb025410d945c
zhangke <zhangke>
2018-08-08 18:25:34 +0800

    SnowFlakeId

--- randomutil/randomutil.go
+++ randomutil/randomutil.go
@@ -1,7 +1,15 @@
 package randomutil
 
-import "math/rand"
-
+import (
+	"math/rand"
+	"fmt"
+	"sync"
+	"github.com/bwmarrin/snowflake"
+)
+var (
+	node *snowflake.Node
+	once sync.Once
+)
 func RandomString(l int) string {
 	bytes := make([]byte, l)
 	for i := 0; i < l; i++ {
@@ -11,4 +19,25 @@ func RandomString(l int) string {
 }
 func RandInt(min int, max int) int {
 	return min + rand.Intn(max-min)
+}
+
+
+//获取db  id int64
+func GetSnowFlakeId(randomId int64) int64 {
+	once.Do(func() {
+		getSnowFlake(randomId)
+	})
+	id := node.Generate()
+	return id.Int64()
+}
+
+
+func getSnowFlake(randomId int64) {
+	node1, err := snowflake.NewNode(int64(randomId))
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		node = node1
+	}
 }
